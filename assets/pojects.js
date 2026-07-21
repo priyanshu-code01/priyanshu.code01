99d6530a5bafcc1b4af773ddcfb160b7734a6c69
Priyanshu Singh <priyanshu.code01@gmail.com>
2026-07-21 12:31:35 +0530

    update resume and projects

--- assets/pojects.js
+++ assets/pojects.js
@@ -1,9 +1,27 @@
 export const workData = [
   {
+    title: "Klimate Weather Website",
+    description:
+      "A modern weather application built with React, TypeScript, and Tailwind CSS that provides real-time weather conditions, hourly and daily forecasts, air quality, and location-based weather insights through the OpenWeather API with a clean and responsive user interface.",
+    bgImage: "/work-1.png",
+    tags: [
+      "React",
+      "TypeScript",
+      "Rechart",
+      "Shadcn",
+      "Tailwind CSS",
+      "OpenWeather API",
+      "Vite",
+    ],
+    projectLink: "https://openKlimate.vercel.app/",
+    viewCode: "https://github.com/priyanshu-code01/Klimate",
+    textColor: "#222222",
+  },
+  {
     title: "GitHub Profile Explorer",
     description:
       "A modern GitHub profile explorer built with React and Tailwind CSS that allows users to search GitHub usernames and view profile details, repositories, followers, and account information in a clean UI using the GitHub REST API.",
-    bgImage: "/work-1.png",
+    bgImage: "/work-2.png",
     tags: ["React", "Tailwind CSS", "GitHub REST API", "Vite"],
     projectLink: "https://gitprofile-dev.vercel.app/",
     viewCode: "https://github.com/priyanshu-code01/gitprofile.dev",
@@ -13,7 +31,7 @@ export const workData = [
     title: "PrimePick – E-Commerce Web Application",
     description:
       "A modern React-based e-commerce application featuring product listing, search, category collections, cart management, and order placement. Implemented global state management using Context API for cart and shop data, with reusable components and responsive UI for a smooth shopping experience.",
-    bgImage: "/work-2.png",
+    bgImage: "/work-3.png",
     tags: ["React", "Tailwind CSS", "useContext", "react-router"],
     projectLink: "https://prime-pick-dun.vercel.app/",
     viewCode: "https://github.com/priyanshu-code01/PrimePick",
@@ -23,20 +41,10 @@ export const workData = [
     title: "Advance Task Manager",
     description:
       "I built a clean and responsive Task Manager Web App using React.js, where users can easily manage their daily tasks with real-time statistics and smooth interactions.",
-    bgImage: "/work-3.png",
+    bgImage: "/work-4.png",
     tags: ["React", "Context API", "Local Storage", "Tailwind"],
     projectLink: "https://advancetaskmanager.vercel.app/",
     viewCode: "https://github.com/priyanshu-code01/Task-Manager",
     textColor: "#7BF0A7",
-  },
-  {
-    title: "Calculator App",
-    description:
-      "An interactive calculator application developed using HTML, CSS, and vanilla JavaScript.",
-    bgImage: "/work-4.png",
-    tags: ["HTML5", "CSS", "JavaScript"],
-    projectLink: "https://calculator-snowy-nine-85.vercel.app/",
-    viewCode: "https://github.com/priyanshu-code01/Calculator",
-    textColor: "#00EAFF",
   },
 ];
